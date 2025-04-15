b76cc24652454613685a4673278a7bf571c3bf61
Gideon Bühler <gideonbuehler18@gmail.com>
2025-04-16 00:04:25 +0200

    clean up camera

--- src/camera.rs
+++ src/camera.rs
@@ -37,16 +37,20 @@ impl Camera {
         }
     }
 
-    pub fn proj_view_matrix(&self) -> Matrix4<f32> {
+    pub fn direction(&self) -> Vector3<f32> {
         let (sin_pitch, cos_pitch) = self.pitch.0.sin_cos();
         let (sin_yaw, cos_yaw) = self.yaw.0.sin_cos();
+        Vector3::new(cos_pitch * sin_yaw, sin_pitch, -cos_pitch * cos_yaw).normalize()
+    }
 
-        let view_mat = Matrix4::look_to_rh(
-            self.position,
-            Vector3::new(cos_pitch * sin_yaw, sin_pitch, -cos_pitch * cos_yaw).normalize(),
-            Vector3::unit_y(),
-        );
+    pub fn forward(&self) -> Vector3<f32> {
+        let (sin_pitch, cos_pitch) = self.pitch.0.sin_cos();
+        let (sin_yaw, cos_yaw) = self.yaw.0.sin_cos();
+        Vector3::new(cos_pitch * sin_yaw, 0.0, -cos_pitch * cos_yaw).normalize()
+    }
 
+    pub fn proj_view_matrix(&self) -> Matrix4<f32> {
+        let view_mat = Matrix4::look_to_rh(self.position, self.direction(), Vector3::unit_y());
         let proj_mat = cgmath::perspective(self.fovy, self.aspect, self.znear, self.zfar);
 
         OPENGL_TO_WGPU_MATRIX * proj_mat * view_mat
